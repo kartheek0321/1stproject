d01784d560a945ea3b2cda4203bb6ee1d7674181
kumari234 <106689731+kumari234@users.noreply.github.com>
2022-11-22 20:46:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "officelapkey" 
+    default     =  "mary_jessie" 
 }
 
 variable "instance_type" { 

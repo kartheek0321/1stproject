e86311ebb0a1db353b628c771bc05732af6c32c3
kumari234 <106689731+kumari234@users.noreply.github.com>
2023-04-23 23:07:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "proj-kk"
+    bucket = "kk-mj"
     key = "main"
     region = "ap-south-1"
-    dynamodb_table = "projDB"
+    dynamodb_table = "kkdb"
   }
 }
